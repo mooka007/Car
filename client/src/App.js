896effab94937b836d6b11322c7e669804be532a
Mooka <47282413+mooka007@users.noreply.github.com>
2024-01-27 02:11:40 +0100

    creating header o home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,41 @@
 
 import './App.css';
+import Header from "./components/Home/Header";
+import Home from "./pages/Home/Home"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  createRoutesFromElements,
+  Route,
+  ScrollRestoration,
+} from "react-router-dom";
+
+const Layout = () => {
+  return ( 
+    <div>
+      <Header />
+      {/* <Footer /> */}
+    </div>
+  )
+}
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />}></Route>
+      </Route>
+    </Route>
+  )
+  );
 
 function App() {
   return (
-    <div>
-      holl
+    <div className="font-bodyFont">
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        // <div className="w-full mx-auto">
+
+        // </div>
+        <h1 className="text-3xl font-bold text-red-500 bg-blue-500">
+            Home page22222
+        </h1>
+    )
+}
+export default Home;
